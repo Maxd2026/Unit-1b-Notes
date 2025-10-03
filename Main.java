9b819a53ac788cc09e3913c9d5462d83130cf623
Maxd2026 <maxwell.dolan2026@gbwl.org>
2025-10-03 12:42:53 +0000

    1

--- Main.java
+++ Main.java
@@ -1,4 +1,16 @@
+/**
+ * @author Max D
+ * @since 3 Oct 2025
+ * @version 1.0
+ * Notes for topics 1.8-1.15
+ */
 public class Main {
+
+   /**
+    * main METHOD is the entry point of the program
+    * @param input is an array of Strings
+    * @return void means NO OUTPUT
+    */
 
    public static void main(String []args) {
       System.out.println("Hello World");
