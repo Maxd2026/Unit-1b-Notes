e20ff5cd7dec085d57f68dd1ac43e608d426999a
Maxd2026 <maxwell.dolan2026@gbwl.org>
2025-10-14 15:09:16 +0000

    3

--- Main.java
+++ Main.java
@@ -20,8 +20,40 @@ public class Main {
 
       System.out.println("Hello World");
 
+      // Strings are OBJECTS from the String CLASS
+      // containing a SEQUENCE OF CHARACTERS
+      // indexed starting from 0
+
+      // Create String objects in 2 ways
+      // 1. Call the String() CONSTRUCTOR
+      String str1 = new String("Com Sci");
+      // 2. They made a SHORTCUT to set up Strings
+      String str2 = "AP Java";
 
 
-      
+      // Call String METHODS on a specific String OBJECT
+      int str1Len = str1.length();
+      int str2Len = str2.length();
+      System.out.println("Length 1: " + str1Len); // CONCATENATE Strings
+      System.out.println("Length 2: " + str2Len);
+      // .length() returns the NUMBER OF CHARACTERS
+      // spaces are included!
+
+      // .substring() is an OVERLOADED method(2 versions)
+      // that returns PART of a larger String
+      String message = "APCS is awesome!";
+
+      String firstWord = message.substring(0, 3);
+      System.out.println(firstWord);
+      // .substring(int from, int to) DOES NOT INCLUDE
+      // the character at index to
+
+      String lastWord = message.substring(8);
+      System.out.println(lastWord);
+
+
+
+
+
    }
 }
